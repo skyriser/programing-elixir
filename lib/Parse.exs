a8d12bf9b43884b9e6dd611484dec2720b917cff
Shin-ichi Ueda <skyriser@levolution.info>
2019-01-17 18:50:38 +0900

    StringsAndBinaries-1

--- lib/Parse.exs
+++ lib/Parse.exs
@@ -10,4 +10,20 @@ defmodule Parse do
   defp _number_digits([non_digit | _], _) do
     raise "Invalid digit '#{non_digit}'"
   end
+
+  # StringsAndBinaries-1
+  # スペース = 32, チルダ = 126
+  # iex(5)> Parse.number(' ')
+  # ** (RuntimeError) Invalid digit '32'
+  # iex(5)> Parse.number('~')
+  # ** (RuntimeError) Invalid digit '126'
+  # Parse.printable?('ASCII STRING_')
+  # true
+  # iex(11)> Parse.printable?('NON ASCII あ')
+  # false
+  def printable?(strings), do: _printable?(strings)
+  defp _printable?([head | _tail]) when head < 32, do: false
+  defp _printable?([head | _tail]) when head > 126, do: false
+  defp _printable?([_head | tail]), do: _printable?(tail)
+  defp _printable?(_char), do: true
 end
